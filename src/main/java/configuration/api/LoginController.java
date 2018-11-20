369d97647e90247a1ffd4ff9cc10a879c8723b91
Philippe Spino <Philippe.spino@usherbrooke.ca>
2018-11-20 13:05:40 -0500

    Updated CORS and redirect configurations to work with frontend

--- src/main/java/configuration/api/LoginController.java
+++ src/main/java/configuration/api/LoginController.java
@@ -1,7 +1,14 @@
 package configuration.api;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -9,7 +16,11 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class LoginController {
 
     @RequestMapping(value = Routes.LOGIN_ROUTE, method = GET)
-    public void login() {
+    public ResponseEntity<Object> login() throws URISyntaxException {
+        URI frontEnd = new URI("http://localhost:8080/home");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(frontEnd);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
 
     }
 }
